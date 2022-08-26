41bb2e4f915b50d4928793d5c76e6454473df33c
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2022-08-26 22:54:51 +0700

    update new

--- app/src/main/java/com/rifqipadisiliwangi/tiptime/MainActivity.kt
+++ app/src/main/java/com/rifqipadisiliwangi/tiptime/MainActivity.kt
@@ -2,10 +2,48 @@ package com.rifqipadisiliwangi.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupListener()
+    }
+
+    fun setupListener(){
+        val et_cost_service = findViewById<EditText>(R.id.et_cost_service)
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+        val swRound = findViewById<Switch>(R.id.swRound)
+        val rgService_rules = findViewById<RadioGroup>(R.id.rgService_rules)
+        val tvAmount = findViewById<TextView>(R.id.tvAmount)
+        var rbService : RadioButton?= null
+
+        rgService_rules.setOnCheckedChangeListener{radioGroup, i ->
+            rbService = findViewById(i)
+        }
+
+        btnCalculate.setOnClickListener{
+            var total= calculateTip(et_cost_service.text.toString().toDouble(), rbService?.text.toString())
+            if (swRound.isChecked){
+                total = total.roundToInt().toDouble()
+            }
+            tvAmount.text ="Total : $total"
+            AlertDialog.Builder(this).setTitle("Tip").setMessage("Total : $total").setNeutralButton("OK", null).show()
+        }
+
+    }
+
+    fun calculateTip(cost : Double, service : String): Double{
+        val tip = when(service){
+            "Amazing (20%)" ->cost *0.2
+            "Good (18%)" ->cost *0.18
+            "Ok (15%)" ->cost *0.15
+            else -> 0.0
+        }
+        return tip
     }
 }
